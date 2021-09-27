350e4cf5c0385dd876aff8815efea45394e98094
Nick <nsagias@gmail.com>
2021-09-27 19:02:31 -0400

    Add urls_news and update express_server with path

--- express_server.js
+++ express_server.js
@@ -23,6 +23,9 @@ app.get("/urls",(req, res)=>{
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(urlDatabase[req.params.shortURL]);
