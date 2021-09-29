a8279696ac67b621def98ee09792c86b8d5ccc0c
Nick <nsagias@gmail.com>
2021-09-28 21:16:39 -0400

    Add morgan middle wear

--- express_server.js
+++ express_server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const morgan = require('morgan');
+
+app.use(morgan('short'));
+
 const bodyParser = require("body-parser");
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
