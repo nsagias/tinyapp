1c875520d83e6e310e3b62a2c1b594814dbbe75a
Nick <nsagias@gmail.com>
2021-10-02 13:00:11 -0400

    Add urslForUserTest

---
+++ test/urlsForUserTest.js
@@ -0,0 +1,84 @@
+const { assert } = require('chai');
+const bcrypt = require('bcryptjs');
+const { urlsForUser } = require('../helpers.js');
+
+const userDB = {
+  "815bd08a": {
+    id: "815bd08a",
+    name: "red",
+    email: "red@example.com",
+    password: bcrypt.hashSync('red', 10)
+  },
+  "ec3bdf7a": {
+    id: "ec3bdf7a",
+    name: "green",
+    email: "green@example.com",
+    password: bcrypt.hashSync('green', 10)
+  },
+  "1f1ffea1": {
+    id: "1f1ffea1",
+    name: "blue",
+    email: "blue@example.com",
+    password: bcrypt.hashSync('blue', 10)
+  }
+};
+
+
+let urlDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "1f1ffea1",
+  },
+  "9sm5xk": {
+    longURL: "http://www.google.com",
+    userID: "1f1ffea1",
+  },
+  "9sm511": {
+    longURL: "http://www.bingo.com",
+    userID: "815bd08a",
+  },
+  "c2k511": {
+    longURL: "http://www.yahoo.com",
+    userID: "815bd08a",
+  }
+};
+
+
+describe('urlsForUser' , () => {
+  it('should return user 815bd08a short urls', () => {
+    const output = urlsForUser ("815bd08a", urlDatabase);
+    const expectedOutput = {
+      '9sm511': { longURL: 'http://www.bingo.com' },
+      c2k511: { longURL: 'http://www.yahoo.com' }
+    };
+    assert.deepEqual(output , expectedOutput);
+  });
+  it('should return not return 815bd08a short urls', () => {
+    const output = urlsForUser ("815bd08a", urlDatabase);
+    const expectedOutput = {
+      b2xVn2: { longURL: 'http://www.lighthouselabs.ca' },
+      '9sm5xk': { longURL: 'http://www.google.com' }
+    };
+    assert.notDeepEqual(output , expectedOutput);
+  });
+  it('should return empty object if no user or inccorect id', () => {
+    const output = urlsForUser ("815bd0afdsa", urlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(output , expectedOutput);
+  });
+  it('should return empty object if empty string passed in', () => {
+    const output = urlsForUser ("", urlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(output , expectedOutput);
+  });
+  it('should return error if Database is empty ', () => {
+    const output = urlsForUser ("", database);
+    const expectedOutput = {};
+    assert.deepEqual(output , expectedOutput);
+  });
+});
+
+let userId = "1f1ffea1"
+const output = urlsForUser (userId, urlDatabase);
+console.log(output);
+
